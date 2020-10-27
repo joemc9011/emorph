236ed558e48a021097d3e5b6acef3dffb2c8b4f9
ryan-gon <ryanpiunj@gmail.com>
2020-10-27 18:46:57 -0400

    backend js

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,63 @@
 
-// var APIKey = "LhZIQJpQlMA7ZaFACizYRcGvrarAHYuqwiABNw-" +
-//     "_45EaMV5KKeQlHpZNjyt8tSBZBcH4uJjhPwIztopHhIH-Kf5MsszDEobMtFMvhTA3zhXZp44bf7PljOR-3HaPX3Yx";
+var APIKey = "AIzaSyBhD3UUFezfTS4WlRIYhGzEwO_blOC2DYs";
+var query = "restaurants";
+var radius = "1000"
+var lnglat = "36.7783, 119.4179";
+  
 
-// var queryURL = "https://api.yelp.com/v3/businesses/search" + APIKey;
+ var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ query +"&key=" + APIKey +"&location=" +lnglat +"&radius=" +radius;
+console.log(queryURL);
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// })
-//     .then(function (response) {
+// $.ajax({ 
+//   url: "https://limitless-tor-79246.herokuapp.com/cors",
+//   method: "POST",
+//   data: {
+//     url: "",
+//     method: "",
+//     key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+  
+//   },
+//    error: function (req, status, error) {
+//     console.log(error);
+//   }
+// });
 
-//         console.log(queryURL);
-
-//         console.log(response);
-//     });
 
 
-var map;
 
-function initMap(response) {
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+  dataType : "json",
+    contentType: "application/json; charset=utf-8",
+  headers: {
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'application/json',
+
+},
+  success: function (response) {
     console.log(response);
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  },
+    error: function(req, status, error) {
+      console.log(status);
+      console.log(req);
+      console.log(error);
+    }
   });
-}
 
-// iufhohvdo
-var = today
-
+   
+     
 
 
-// drop down for price
+// var map;
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+// // function initMap(response) {
+//     console.log(response);
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
