54ddf67a2744cd8c4c727957983fe64e4501a6a8
joemc9011 <joemc9011@gmail.com>
2020-10-28 16:20:30 -0400

    front

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,141 @@
+// This is our API key
+    // var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
-// var inputStorage = $("#textarea1").localStorage.setItem("location", location.val());
-var priceSelect = $("#priceChoice").val();
+    // // Here we are building the URL we need to query the database
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    //   "q=Bujumbura,Burundi&appid=" + APIKey;
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
 
-console.log(priceSelect);
-// function initMap(response) {
-//     console.log(response);
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//   });
-// }
-// // drop down for price
+    function success(pos) {
+      var crd = pos.coords;
 
-//   $(document).ready(function(){
-//     $('select').formSelect();
-//   });
+      var APIKey = "AIzaSyBhD3UUFezfTS4WlRIYhGzEwO_blOC2DYs";
+      var query = "restaurants";
+      var radius = "0"
+      var lnglat = crd;
+      var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + APIKey + "&location=" + crd;
+      console.log(queryURL);
+      $.ajax({
+        url: "https://limitless-tor-79246.herokuapp.com/cors",
+        method: "POST",
+        data: {
+          url: queryURL,
+          method: "",
+          key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+        }
+      })
+        .then(function (response) {
+          console.log(response);
+          // var data = JSON.stringify(response.base)
+          // $("#response").append(data)
+
+        });
+
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options)
+
+    // var APIKey = "AIzaSyBhD3UUFezfTS4WlRIYhGzEwO_blOC2DYs";
+    // var query = "restaurants";
+    // var radius = "1000"
+    // var lnglat = "41.881832, -87.623177";
+    // var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + APIKey + "&location=" + lnglat + "&radius=" + radius;
+    // console.log(queryURL);
+    // $.ajax({
+    //   url: "https://limitless-tor-79246.herokuapp.com/cors",
+    //   method: "POST",
+    //   data: {
+    //     url: queryURL,
+    //     method: "",
+    //     key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+    //   }
+    // })
+    //   .then(function (response) {
+    //     console.log(response);
+    //     // var data = JSON.stringify(response.base)
+    //     // $("#response").append(data)
+
+    //   });
+
+    // This is our API key
+    // var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+    // // Here we are building the URL we need to query the database
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    //   "q=Bujumbura,Burundi&appid=" + APIKey;
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+      var crd = pos.coords;
+
+      var APIKey = "AIzaSyBhD3UUFezfTS4WlRIYhGzEwO_blOC2DYs";
+      var query = "restaurants";
+      var radius = "0"
+      var lnglat = crd;
+      var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + APIKey + "&location=" + crd;
+      console.log(queryURL);
+      $.ajax({
+        url: "https://limitless-tor-79246.herokuapp.com/cors",
+        method: "POST",
+        data: {
+          url: queryURL,
+          method: "",
+          key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+        }
+      })
+        .then(function (response) {
+          console.log(response);
+          // var data = JSON.stringify(response.base)
+          // $("#response").append(data)
+
+        });
+
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options)
+
+    // var APIKey = "AIzaSyBhD3UUFezfTS4WlRIYhGzEwO_blOC2DYs";
+    // var query = "restaurants";
+    // var radius = "1000"
+    // var lnglat = "41.881832, -87.623177";
+    // var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + APIKey + "&location=" + lnglat + "&radius=" + radius;
+    // console.log(queryURL);
+    // $.ajax({
+    //   url: "https://limitless-tor-79246.herokuapp.com/cors",
+    //   method: "POST",
+    //   data: {
+    //     url: queryURL,
+    //     method: "",
+    //     key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+    //   }
+    // })
+    //   .then(function (response) {
+    //     console.log(response);
+    //     // var data = JSON.stringify(response.base)
+    //     // $("#response").append(data)
+
+    //   });
